86ffb6cea931c8777b0eeefa10ea34aa4ed8ccd9
Kelechi Otugbuali <hilakechi@gmail.com>
2023-07-18 10:39:55 +0100

    Added the Api keys so Netlify can read from it

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:'AIzaSyArHPKnn1iunWAdlzTvV1yfBWbQigMxvYc',
-  authDomain:'netflixclone-d98aa.firebaseapp.com',
-  projectId:'netflixclone-d98aa',
-  storageBucket:'netflixclone-d98aa.appspot.com',
-  messagingSenderId:'900123001871',
-  appId:'1:900123001871:web:f87f1e3396f69649fe8f7a'
+  apiKey:"AIzaSyArHPKnn1iunWAdlzTvV1yfBWbQigMxvYc",
+  authDomain:"netflixclone-d98aa.firebaseapp.com",
+  projectId:"netflixclone-d98aa",
+  storageBucket:"netflixclone-d98aa.appspot.com",
+  messagingSenderId:"900123001871",
+  appId:"1:900123001871:web:f87f1e3396f69649fe8f7a"
 };
 
 const app = initializeApp(firebaseConfig);
