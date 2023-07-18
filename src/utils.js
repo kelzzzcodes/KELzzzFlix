c2caf69832d5a7bf47f6204075b3b6a203578a6e
Kelechi Otugbuali <hilakechi@gmail.com>
2023-07-18 23:08:16 +0100

    function handleSplit modified in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,17 @@
+export function handleSplit(str) {
+  const truncateString = str.split(' ')
 
-export function handleSplit (str) {
-    if (str.includes(":")) {
-        return str.split(":")[0];
-      } else if (str.includes("-")) {
-        return str.split("-")[0];
-      } else {
-        return str;
+    if (truncateString[0].length > 5) {
+      let NewString = truncateString[0]
+
+      if(NewString.includes(":")){
+        return NewString.split(":")[0]
+     }else if(NewString.includes('-'))
+         return NewString.split("-")[0]
+      else{
+        return NewString
       }
-  }
-
+    } else {
+      return`${truncateString[0]} ${truncateString[1]}`
+    }
+}
