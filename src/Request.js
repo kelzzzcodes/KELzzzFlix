75ecbd559e838aa41a6cbe87adeb50df1d73f63f
Kelechi Otugbuali <hilakechi@gmail.com>
2023-07-18 10:28:46 +0100

    initial commit

--- src/Request.js
+++ src/Request.js
@@ -1,4 +1,4 @@
-const key =process.env.REACT_APP_IMDB_API_KEY
+const key =d9e54eec050779a6f6ee5266348d3767
 
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
