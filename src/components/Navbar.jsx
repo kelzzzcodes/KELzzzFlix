6fb72685921d5d18ca7e144ce8763896a8f5f641
Kelechi Otugbuali <hilakechi@gmail.com>
2023-07-19 13:13:53 +0100

    bg-color change on scroll feat  Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,19 @@ import Search from './Search'
 import RightNav from './RightNav'
 
 const Navbar = () => {
+  const [navBgColor,setNavBgColor] = useState(false)
+  const handleBgColor =()=>{
+    if(window.scrollY >= 70){
+      setNavBgColor(true)
+    }else{
+      setNavBgColor(false)
+    }
+
+  }
+  window.addEventListener('scroll',handleBgColor)
+
   return (
-    <div className="w-full flex items-center justify-between px-4 py-10 lg:py-16 z-[100]  fixed h-[70px]  border-b-2  border-gray-50/20">
+    <div className={`w-full flex items-center justify-between px-4 py-10 lg:py-16 z-[100]  fixed h-[70px]  border-b-2  border-gray-50/20 ${navBgColor ? 'bg-black/90' : ' bg-transparent'}`}>
       <Link to="/">
         <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
           KELzzzFlix
