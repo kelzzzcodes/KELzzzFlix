92940ea0282dc3a6dc18fa771563c5dfc991afdc
Kelechi Otugbuali <hilakechi@gmail.com>
2023-09-27 10:15:33 +0100

    new commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,29 +11,27 @@ const Banner = () => {
   useEffect(() => {
     axios.get(requests.requestPopular).then((response) => {
       setMovies(response.data.results)
-     
     })
-
   }, [])
 
   return (
-    <div id='/' className="w-full h-[800px] text-white">
+    <div id="/" className="w-full h-[700px] text-white">
       <div className="w-full h-full">
-        <div className="w-full h-full absolute bottom-32 bg-gradient-to-r from-black"></div>
+        <div className="w-full h-[700px] absolute bottom-30 bg-gradient-to-r from-black"></div>
         <img
           className="w-full h-full object-cover "
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
         <div className="absolute flex flex-col gap-8 w-[70%] md:w-full top-[20%] p-4 md:px-16 lg:px-32 ">
-          <h1 className="text-xl mt-8 md:text-3xl lg:text-5xl font-bold">{movie?.title}</h1>
+          <h1 className="text-xl mt-8 md:text-3xl lg:text-5xl font-bold">
+            {movie?.title}
+          </h1>
           <p className="w-full line-clamp-3 md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200  text-lg md:text-xl">
             {movie?.overview}
           </p>
           <div className="flex flex-col md:flex-row md:items-center gap-2 text-stone-400 text-sm md:text-xl">
-            <p>
-              Released: {movie?.release_date}
-            </p>
+            <p>Released: {movie?.release_date}</p>
             <p className="md:border-l-2  md:border-[#E50916]">
               <span className="md:ml-2">Rating: {movie?.vote_average}</span>
             </p>
